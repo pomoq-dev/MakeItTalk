8df64124a3b9e4412103e8bac38cd8e719227474
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:57:33 +0300

    Change image converter save file names

--- image_converter.py
+++ image_converter.py
@@ -5,13 +5,18 @@ from PIL import Image
 
 def convert_images(directory):
     file_names = os.listdir(directory)
+    cnt = 0
     for file_name in file_names:
         if file_name.endswith('.jpeg') or file_name.endswith('.png') or file_name.endswith('.jpg'):
             print(file_name)
             just_name = '.'.join(file_name.split('.')[:-1])
-            img = Image.open(os.path.join(directory, file_name))
+            original_path = os.path.join(directory, file_name)
+            img = Image.open(original_path)
             rgb_img = img.convert('RGB')
             rgb_img = rgb_img.resize((256, 256))
-            rgb_img.save(os.path.join(directory, '{}.jpg'.format(just_name)))
-            if not file_name.endswith('.jpg'):
+            res_name = 'hobana_image_{}'.format(cnt)
+            cnt += 1
+            res_path = os.path.join(directory, '{}.jpg'.format(res_name))
+            rgb_img.save(res_path)
+            if res_path != original_path:
                 os.remove(os.path.join(directory, file_name))
