b39ed0523742ebc180844746eba12ec9918a9b21
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:57:33 +0300

    Add new notebook with download directory from drive

--- image_converter.py
+++ image_converter.py
@@ -2,14 +2,15 @@ import os
 
 from PIL import Image
 
-EXAMPLES_DIR = 'examples'
 
-file_names = os.listdir(EXAMPLES_DIR)
-for file_name in file_names:
-    if file_name.endswith('.jpeg') or file_name.endswith('.png'):
-        print(file_name)
-        just_name = '.'.join(file_name.split('.')[:-1])
-        img = Image.open(os.path.join(EXAMPLES_DIR, file_name))
-        rgb_img = img.convert('RGB')
-        rgb_img.save(os.path.join(EXAMPLES_DIR, '{}.jpg'.format(just_name)))
-        os.remove(os.path.join(EXAMPLES_DIR, file_name))
+def convert_images(directory):
+    file_names = os.listdir(directory)
+    for file_name in file_names:
+        if file_name.endswith('.jpeg') or file_name.endswith('.png'):
+            print(file_name)
+            just_name = '.'.join(file_name.split('.')[:-1])
+            img = Image.open(os.path.join(EXAMPLES_DIR, file_name))
+            rgb_img = img.convert('RGB')
+            rgb_img = rgb_img.resize((256, 256))
+            rgb_img.save(os.path.join(EXAMPLES_DIR, '{}.jpg'.format(just_name)))
+            os.remove(os.path.join(EXAMPLES_DIR, file_name))
