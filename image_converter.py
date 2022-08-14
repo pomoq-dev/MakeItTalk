10a5a3b3f8457fecd02b2721de87280bdb29cf04
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:57:33 +0300

    Fix image_converter

--- image_converter.py
+++ image_converter.py
@@ -9,8 +9,8 @@ def convert_images(directory):
         if file_name.endswith('.jpeg') or file_name.endswith('.png'):
             print(file_name)
             just_name = '.'.join(file_name.split('.')[:-1])
-            img = Image.open(os.path.join(EXAMPLES_DIR, file_name))
+            img = Image.open(os.path.join(directory, file_name))
             rgb_img = img.convert('RGB')
             rgb_img = rgb_img.resize((256, 256))
-            rgb_img.save(os.path.join(EXAMPLES_DIR, '{}.jpg'.format(just_name)))
-            os.remove(os.path.join(EXAMPLES_DIR, file_name))
+            rgb_img.save(os.path.join(directory, '{}.jpg'.format(just_name)))
+            os.remove(os.path.join(directory, file_name))
