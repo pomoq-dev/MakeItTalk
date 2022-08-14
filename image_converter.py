4e70ce90372a0bffaffacac79ff817cd67310141
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:57:31 +0300

    Add new images

--- image_converter.py
+++ image_converter.py
@@ -8,7 +8,7 @@ file_names = os.listdir(EXAMPLES_DIR)
 for file_name in file_names:
     if file_name.endswith('.jpeg') or file_name.endswith('.png'):
         print(file_name)
-        just_name = file_name.split('.')[0]
+        just_name = '.'.join(file_name.split('.')[:-1])
         img = Image.open(os.path.join(EXAMPLES_DIR, file_name))
         rgb_img = img.convert('RGB')
         rgb_img.save(os.path.join(EXAMPLES_DIR, '{}.jpg'.format(just_name)))
